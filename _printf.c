3b7977cc76452c160219cc63f262a112b0000789
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-10-26 01:51:40 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,38 +9,63 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, var = 0;
-	va_list v_ls;
-	buffer *buf;
+	va_list arg_list;
+	int i_fmt, i_struct, flag, ret_val, ret_flag;
+	fmt_chars p_fmt[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int},
+		{"b", print_binary},
+		{NULL, NULL}
+	};
 
-	buf = buf_new();
-	if (buf == NULL)
-		return (-1);
+	flag = 0;
+	ret_val = 0;
+	va_start(arg_list, format);
+
+	for (i_fmt = 0; format != NULL && format[i_fmt] != '\0'; i_fmt++)
+	{
+		if (format[i_fmt] == '%' && format[i_fmt + 1] != '%')
+		{
+			for (i_struct = 0; i_struct < 5; i_struct++)
+			{
+				if (format[i_fmt + 1] == *p_fmt[i_struct].id)
+				{
+					ret_flag = p_fmt[i_struct].f(arg_list);
+					if (ret_flag == -1)
+						return (-1);
+					ret_val += ret_flag;
+					flag = 1;
+					i_fmt++;
+				}
+			}
+			if (flag == 0)
+			{
+				ret_flag = _putchar(format[i_fmt]);
+				if (ret_flag == -1)
+					return (-1);
+				ret_val += ret_flag;
+			}
+		}
+		else if (format[i_fmt] == '%' && format[i_fmt + 1] == '%')
+		{
+			ret_flag = _putchar('%');
+			if (ret_flag == -1)
+				return (-1);
+			ret_val += ret_flag;
+			i_fmt++;
+		}
+		else
+		{
+			ret_flag = _putchar(format[i_fmt]);
+			if (ret_flag == -1)
+				return (-1);
+			ret_val += ret_flag;
+		}
+	}
+	va_end(arg_list);
 	if (format == NULL)
 		return (-1);
-	va_start(v_ls, format);
-	while (format[i])
-	{
-		buf_wr(buf);
-		if (format[i] == '%')
-		{
-			var = opid(buf, v_ls, format, i);
-			if (var < 0)
-			{
-				i = var;
-				break;
-			}
-			i += var;
-			continue;
-		}
-		buf->str[buf->index] = format[i];
-		buf_inc(buf);
-		i++;
-	}
-	buf_write(buf);
-	if (var >= 0)
-		i = buf->overflow;
-	buf_end(buf);
-	va_end(v_ls);
-	return (i);
+	return (ret_val);
 }
